be5c7bf8a54973f02cf5d1d2ea4145fff724f981
Charlize <extrabutterychar@gmail.com>
2024-06-17 01:35:44 -0700

    Finished Most of Top Bar Attempting to Add Search Icon

---
+++ my-app/src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header_left">
+                {/* <img src="" alt="" /> */}
+                <h2><a href="#">Game</a></h2>
+            </div>
+            <div className="header_center">
+                <ul className="header_lists">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href=""> Blog</a></li>
+                    <li><a href=""> Contract</a></li>
+
+                </ul>
+            </div>
+            <div className="header_right">
+                <button>Help me Find</button>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
