b2a0a4addd68e82a0500725a31601d4e90c942c1
Charlize <extrabutterychar@gmail.com>
2024-06-18 22:04:24 -0700

    Got Axios Get to Work - Mapping has not been set up yet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
+import axios from 'axios';
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home'
-
 import GamePage from './Pages/GamePage'
 
 function App() {
+  function jsonList(){
+    const jsonArray = [];
+    const populateData = (data) => {jsonArray.push(data)};
+    axios.get(`https://hl2qrc5x51.execute-api.us-west-1.amazonaws.com/dev/boardgames`)
+    .then(response => {
+    
+      // handle the response
+        const jsonData = response.data; // object file
+        // console.log(jsonData);
+        const result = jsonData.boardgames.result;
+        for (let i = 0; i < result.length; i++) {
+          populateData(jsonData.boardgames.result[i]); 
+        }
+        // console.log("result: ", result);
+    })
+    console.log("jsonArray: ", jsonArray)
+    return jsonArray;
+  }
+
+  
   return (
     <div>
+      {jsonList()}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}/>
